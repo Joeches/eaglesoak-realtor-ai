15f31e694b4709786c8e813e2d7529e55077cad5
Joeches <77074250+Joeches@users.noreply.github.com>
2025-11-01 17:10:43 -0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,1 +1,22 @@
+// src/main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+/**
+ * StrictMode is enabled for dev-time warnings.
+ * The app uses BrowserRouter; Netlify redirect is set to serve index.html for all routes.
+ */
+const container = document.getElementById("root");
+if (!container) throw new Error("Root container not found");
+
+createRoot(container).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
